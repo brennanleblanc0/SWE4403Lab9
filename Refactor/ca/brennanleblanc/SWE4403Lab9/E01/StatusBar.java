f537b2f0e25ca083fd420e2ff6fd5dd4594cbcfe
brennanleblanc0 <me@brennanleblanc.ca>
2023-03-28 16:10:09 -0300

    Include Override tags

--- Refactor/ca/brennanleblanc/SWE4403Lab9/E01/StatusBar.java
+++ Refactor/ca/brennanleblanc/SWE4403Lab9/E01/StatusBar.java
@@ -2,6 +2,7 @@ package ca.brennanleblanc.SWE4403Lab9.E01;
 
 public class StatusBar extends StockView {
 
+    @Override
     public void show() {
         for (var stock : stocks)
             System.out.println(stock);
--- Refactor/ca/brennanleblanc/SWE4403Lab9/E01/StockListView.java
+++ Refactor/ca/brennanleblanc/SWE4403Lab9/E01/StockListView.java
@@ -2,6 +2,7 @@ package ca.brennanleblanc.SWE4403Lab9.E01;
 
 public class StockListView extends StockView {
 
+    @Override
     public void show() {
         for (var stock : stocks)
             System.out.println(stock);
